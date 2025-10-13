bbab459784c3e40afaf1bec9bfecf31fc61d60f5
João Victor Santos Santana <joao.santana@gbmx.com.br>
2025-10-13 10:53:51 -0300

    projeto pronto?

--- script.js
+++ script.js
@@ -1,45 +1,63 @@
 let posicao = 0;
 let vagaoAtual = 0;
 
+let imagemAddVagaoAdicionada = false;
+let imagemRemoveVagaoAdicionada = false;
+let imagemCorrerVagaoAdicionada = false; // Variável para controle da imagem de mover
+
 function adicionarVagao() {
   if (vagaoAtual >= 4) {
     alert("🚫 Você já adicionou o número máximo de vagões!");
     return;
   }
+
   vagaoAtual++;
+
+  if (!imagemAddVagaoAdicionada) {
+    const imgDiv = document.querySelector('.imagesDiv');
+    const printAddVagao = document.createElement('img');
+    printAddVagao.src = './icones/addVagao.png';
+    printAddVagao.classList.add('printCode');
+    imgDiv.appendChild(printAddVagao);
+    imagemAddVagaoAdicionada = true;
+  }
+
   const idDoVagao = `vagao${vagaoAtual}`;
   const vagaoParaMostrar = document.getElementById(idDoVagao);
+
   if (vagaoParaMostrar) {
     vagaoParaMostrar.classList.remove("invisivel");
   }
 }
 
-
-
 function removerVagao() {
-
   if (vagaoAtual <= 0) {
     alert("🚫 Não há mais vagões para remover!");
     vagaoAtual = 0;
     return;
   }
 
+  if (!imagemRemoveVagaoAdicionada) {
+    const imgDiv = document.querySelector('.imagesDiv');
+    const printRemoveVagao = document.createElement('img');
+    printRemoveVagao.src = './icones/removeVagao.png';
+    printRemoveVagao.classList.add('printCode');
+    imgDiv.appendChild(printRemoveVagao);
+    imagemRemoveVagaoAdicionada = true;
+  }
+
   const idDoVagao = `vagao${vagaoAtual}`;
   const vagaoParaEsconder = document.getElementById(idDoVagao);
-
 
   if (vagaoParaEsconder) {
     vagaoParaEsconder.classList.add("invisivel");
   }
-
 
   vagaoAtual--;
 
   posicao = 0;
   document.getElementById("trem").style.left = "0px";
 }
-
-
 
 function mover() {
   if (vagaoAtual === 0) {
@@ -50,9 +68,24 @@ function mover() {
   const trem = document.getElementById("trem");
   const chegada = document.getElementById("chegada");
 
-  const incremento = (vagaoAtual > 2) ? 40 : 80;
-  posicao += incremento;
+  if (!imagemCorrerVagaoAdicionada) {
+    const imgDiv = document.querySelector('.imagesDiv');
+    const printMoverVagao = document.createElement('img');
+    printMoverVagao.src = './icones/moverVagao.png'; 
+    printMoverVagao.classList.add('printCode');
+    imgDiv.appendChild(printMoverVagao);
+    imagemCorrerVagaoAdicionada = true;
+  }
 
+  let incremento;
+
+  if (vagaoAtual > 2) {
+    incremento = 40;
+  } else {
+    incremento = 80;
+  }
+
+  posicao += incremento;
   trem.style.left = posicao + "px";
 
   const tremRect = trem.getBoundingClientRect();
@@ -61,13 +94,20 @@ function mover() {
   if (tremRect.right >= chegadaRect.left) {
     alert("🎉 Chegou ao destino! O trem vai voltar ao início.");
     posicao = 0;
-    trem.style.left = posicao + "px"; 
-    return; 
+    trem.style.left = posicao + "px";
+
+    const imgDiv = document.querySelector('.imagesDiv');
+    imgDiv.innerHTML = "";
+
+    imagemAddVagaoAdicionada = false;
+    imagemRemoveVagaoAdicionada = false;
+    imagemCorrerVagaoAdicionada = false; 
+
+    return;
   }
 
-   if (posicao >= limiteDireito) {
-     posicao = limiteDireito;
-     trem.style.left = posicao + "px";
-   }
-  
+  if (posicao >= limiteDireito) {
+    posicao = limiteDireito;
+    trem.style.left = posicao + "px";
+  }
 }
